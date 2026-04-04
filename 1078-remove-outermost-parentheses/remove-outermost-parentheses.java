26976f4f49a09b0839a105de632f5093e3e4f1d5
Aryan Dixit <aryan.dixit_cs24@gla.ac.in>
2026-04-04 23:43:59 +0530

    Time: 6 ms (30.35%) | Memory: 43.7 MB (35.70%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(') {
+                
+                if (!stack.isEmpty()) {
+                    result.append(c);
+                }
+                stack.push(c);
+            } 
+            else {
+              
+                stack.pop();
+
+                
+                if (!stack.isEmpty()) {
+                    result.append(c);
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
