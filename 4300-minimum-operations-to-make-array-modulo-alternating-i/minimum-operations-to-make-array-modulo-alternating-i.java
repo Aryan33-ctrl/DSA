dad29aab32fe2ac25d44a5d374ae7a73f6479195
Aryan Dixit <aryan.dixit_cs24@gla.ac.in>
2026-05-24 23:04:02 +0530

    Time: 6 ms (100.00%) | Memory: 44.9 MB (100.00%) - LeetSync

---
+++ 4300-minimum-operations-to-make-array-modulo-alternating-i/minimum-operations-to-make-array-modulo-alternating-i.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int n = nums.length;
+        int[] costEven = new int[k];
+        int[] costOdd = new int[k];
+        
+        for (int i = 0; i < n; i++) {
+            int rem = nums[i] % k;
+            for (int target = 0; target < k; target++) {
+                int diff = Math.abs(rem - target);
+                int cost = Math.min(diff, k - diff);
+                if (i % 2 == 0) {
+                    costEven[target] += cost;
+                } else {
+                    costOdd[target] += cost;
+                }
+            }
+        }
+        
+        int minOps = Integer.MAX_VALUE;
+        for (int x = 0; x < k; x++) {
+            for (int y = 0; y < k; y++) {
+                if (x != y) {
+                    minOps = Math.min(minOps, costEven[x] + costOdd[y]);
+                }
+            }
+        }
+        
+        return minOps;
+    }
+}
