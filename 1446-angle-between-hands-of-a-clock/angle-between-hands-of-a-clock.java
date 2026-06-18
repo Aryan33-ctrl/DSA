3824be423f0f753924d457761be0c081d6248a36
Aryan Dixit <aryan.dixit_cs24@gla.ac.in>
2026-06-18 19:49:21 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (23.81%) - LeetSync

---
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        hour%=12;
+
+        double hourangle=hour*30+minutes*0.5;
+
+        double minuteangle=minutes*6;
+
+        double diff=Math.abs(hourangle-minuteangle);
+
+        return Math.min(diff,360-diff);
+    }
+}
