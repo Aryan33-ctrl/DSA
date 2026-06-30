7a54904b97f70735be25b9eb118c9713867694ef
Aryan Dixit <aryan.dixit_cs24@gla.ac.in>
2026-07-01 00:17:18 +0530

    Time: 22 ms (41.29%) | Memory: 50.2 MB (95.03%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        map.put(0,1);
+
+        int sum=0;
+        int ans=0;
+
+        for(int num:nums){
+            sum+=num;
+
+            ans+=map.getOrDefault(sum-goal,0);
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return ans;
+        
+    }
+}
